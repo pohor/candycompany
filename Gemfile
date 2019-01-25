248f9a46a15dd3d0c80adb5f1af4e3d01d5c8965
pohor <ola.pohorecka@gmail.com>
2019-01-25 15:46:22 +0100

    Add thumbnails to posts with minimagic gem, add 'no posts' text where no posts in category

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'feathericon-rails'
 gem 'cloudinary'
 gem 'dotenv'
 gem 'carrierwave', '~> 1.0'
+gem 'mini_magick'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/uploaders/post_cover_uploader.rb
+++ app/uploaders/post_cover_uploader.rb
@@ -3,7 +3,7 @@ class PostCoverUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -31,9 +31,9 @@ class PostCoverUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [250, 250]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

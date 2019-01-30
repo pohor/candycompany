9ae2949b4680f1c2bbf50bba8b46b7bf6443f0f0
pohor <ola.pohorecka@gmail.com>
2019-01-30 12:50:00 +0100

    Added cards to home view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,7 +20,7 @@ class StaticPagesController < ApplicationController
   private
 
   def find_content
-    @posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 2)
+    @posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 9)
     @categories = Category.all
     @ingredients = Ingredient.all
     @users = User.all

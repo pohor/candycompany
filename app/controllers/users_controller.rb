ef629093e5b1389ef691342542d7d975b0983e77
pohor <ola.pohorecka@gmail.com>
2019-01-30 20:27:03 +0100

    Amended views for users and posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def show
--- app/uploaders/post_cover_uploader.rb
+++ app/uploaders/post_cover_uploader.rb
@@ -44,6 +44,10 @@ class PostCoverUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [150, 150]
   end
 
+  version :mikro do
+    process resize_to_fit: [50, 50]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist

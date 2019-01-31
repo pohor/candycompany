ef2e5f453877405fc087db38eed99dba670a8363
pohor <ola.pohorecka@gmail.com>
2019-01-31 22:27:42 +0100

    Fixed comments bug, finished views

--- app/uploaders/post_cover_uploader.rb
+++ app/uploaders/post_cover_uploader.rb
@@ -30,6 +30,7 @@ class PostCoverUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+
   # Create different versions of your uploaded files:
   version :regular do
     process :resize_to_limit => [600,600]
@@ -47,6 +48,7 @@ class PostCoverUploader < CarrierWave::Uploader::Base
   version :mikro do
     process resize_to_fit: [50, 50]
   end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

0883f2fb2fb1e85f29f05331a48c4dd9724c812e
pohor <ola.pohorecka@gmail.com>
2019-02-01 17:03:34 +0100

    Fix posts / users relation

--- db/migrate/20190108194400_add_reference_to_user_posts.rb
+++ db/migrate/20190108194400_add_reference_to_user_posts.rb
@@ -1,6 +1,6 @@
 class AddReferenceToUserPosts < ActiveRecord::Migration[5.2]
   def change
     add_reference :posts, :user
-    add_foreign_key :posts, :user
+    add_foreign_key :posts, :users
   end
 end
